c462e6fc191a038141cfcad5d26833234d414e83
Tarikul711 <Tarikul711@gmail.com>
2020-08-13 10:58:39 +0600

    Add employee model class and registeredin admin

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.email
